8363132b34c54cb86e4a5473b7cb8bc3d335961a
Andrew Goglachev <andru7428@yandex.ru>
2021-02-25 17:26:28 +0500

    Add connect/disconnect timestamp

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,33 @@
 const socket = io({ autoConnect: false });
 var username;
 
+window.onload = () => {
+  var username_input = document.getElementById("username_input");
+  document.getElementById("username_form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (username_input.value) {
+      username = username_input.value;
+      socket.auth = { username };
+      socket.connect();
+      overlay.remove();
+      message_input.focus();
+      var item = document.createElement("li");
+      item.textContent = `[${new Date().toLocaleTimeString()}] ${username} has connected`;
+      messages.appendChild(item);
+    }
+  });
+  username_input.focus();
+
+  var message_input = document.getElementById("message_input");
+  document.getElementById("message_form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (message_input.value) {
+      socket.emit("message", message_input.value);
+      message_input.value = "";
+    }
+  });
+};
+
 socket.on("message", (msg) => {
   addMessage(`${msg.username}: ${msg.text}`);
 });
@@ -19,40 +46,17 @@ socket.on("user connected", (user) => {
   item.id = user.userID;
   item.textContent = user.username;
   users.appendChild(item);
-  addMessage(`${user.username} has connected`);
+  addMessage(
+    `[${new Date().toLocaleTimeString()}] ${user.username} has connected`
+  );
 });
 
 socket.on("user disconnected", (user) => {
   document.getElementById(user.userID).remove();
-  addMessage(`${user.username} has disconnected`);
+  addMessage(
+    `[${new Date().toLocaleTimeString()}] ${user.username} has disconnected`
+  );
 });
-
-window.onload = () => {
-  var username_input = document.getElementById("username_input");
-  document.getElementById("username_form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (username_input.value) {
-      username = username_input.value;
-      socket.auth = { username };
-      socket.connect();
-      overlay.remove();
-      message_input.focus();
-      var item = document.createElement("li");
-      item.textContent = `${username} has connected`;
-      messages.appendChild(item);
-    }
-  });
-  username_input.focus();
-
-  var message_input = document.getElementById("message_input");
-  document.getElementById("message_form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (message_input.value) {
-      socket.emit("message", message_input.value);
-      message_input.value = "";
-    }
-  });
-};
 
 function addMessage(text) {
   var item = document.createElement("li");
