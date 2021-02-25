8fc92f0b55ad10e62d746929cdb7e64ca93177be
Andrew Goglachev <andru7428@yandex.ru>
2021-02-25 11:50:09 +0500

    Add username form overlay

--- public/chat.js
+++ public/chat.js
@@ -2,10 +2,7 @@ const socket = io({ autoConnect: false });
 var username;
 
 socket.on("message", (msg) => {
-  var item = document.createElement("li");
-  item.textContent = `${msg.username}: ${msg.text}`;
-  messages.appendChild(item);
-  messages.scrollTo(0, messages.scrollHeight);
+  addMessage(`${msg.username}: ${msg.text}`);
 });
 
 socket.on("users", (io_users) => {
@@ -22,18 +19,12 @@ socket.on("user connected", (user) => {
   item.id = user.userID;
   item.textContent = user.username;
   users.appendChild(item);
-
-  var item = document.createElement("li");
-  item.textContent = `${user.username} has connected`;
-  messages.appendChild(item);
+  addMessage(`${user.username} has connected`);
 });
 
 socket.on("user disconnected", (user) => {
   document.getElementById(user.userID).remove();
-
-  var item = document.createElement("li");
-  item.textContent = `${user.username} has disconnected`;
-  messages.appendChild(item);
+  addMessage(`${user.username} has disconnected`);
 });
 
 window.onload = () => {
@@ -41,10 +32,17 @@ window.onload = () => {
   document.getElementById("username_form").addEventListener("submit", (e) => {
     e.preventDefault();
     if (username_input.value) {
-      socket.auth = { username: username_input.value };
+      username = username_input.value;
+      socket.auth = { username };
       socket.connect();
+      overlay.remove();
+      message_input.focus();
+      var item = document.createElement("li");
+      item.textContent = `${username} has connected`;
+      messages.appendChild(item);
     }
   });
+  username_input.focus();
 
   var message_input = document.getElementById("message_input");
   document.getElementById("message_form").addEventListener("submit", (e) => {
@@ -55,3 +53,10 @@ window.onload = () => {
     }
   });
 };
+
+function addMessage(text) {
+  var item = document.createElement("li");
+  item.textContent = text;
+  messages.appendChild(item);
+  messages.scrollTo(0, messages.scrollHeight);
+}
